f29448800d5006a14506da13dff3ef754ec117d0
AUNT1oN256 <anton.mironets@gmail.com>
2024-11-16 15:35:51 +0200

    print clock for site

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 function updateClock(){
-
+    const now = new Date();
+    const hour = String(now.getHours()).padStart(2, "0");
+    const minutes = String(now.getMinutes()).padStart(2, "0");
+    const seconds = String(now.getSeconds()).padStart(2, "0");
+    document.getElementById('clock').textContent = `${hour}:${minutes}:${seconds}`;
 }
 
-document.getElementById('clock').textContent = new Date()
+setInterval(updateClock, 1000);
